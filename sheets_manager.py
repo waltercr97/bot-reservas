ddfe282bd8eb5b2fa60fde81428130a172ea69d5
Walter Corrales <waltercartin41@gmail.com>
2025-06-23 13:11:08 -0600

    sheets_manager modified

--- sheets_manager.py
+++ sheets_manager.py
@@ -2,18 +2,17 @@
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
+import os
 
 def conectar_sheets():
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive"
-    ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "credentials.json",
-        scope
-    )
-    client = gspread.authorize(creds)
-    return client
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    
+    credentials_json = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json, scope)
+    
+    return gspread.authorize(creds)
 
 def guardar_cita(chat_id,nombre, telefono, fecha, hora):
     hoja = conectar_sheets().open("CitasReservadas").sheet1
